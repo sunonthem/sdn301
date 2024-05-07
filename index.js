218cad2fda5c31b1e6d8f4a2d8bf1fd173b7264d
duong <duongtde170786@fpt.edu.vn>
2024-05-07 13:48:12 +0700

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+var rect= {
+    perimeter : (x,y)=>(2*(x+y)),
+    area : (x,y)=>(x*y)
+}
+function solveRect(l,b ){
+    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+    if(l<=0 || b<=0){
+        console.log("Rectangle dimensions should be greater than zero: l = " + l + ", and b = " + b);
+    }
+    else ( l>= 0 || B>=0)
+    {
+        console.log("perimeter is "+ rect.perimeter(l,b));
+        console.log("area is "+ rect.area(l,b));
+    }
+}
+solveRect(2,4);
+solveRect(3,5);
+solveRect(-3,5);
