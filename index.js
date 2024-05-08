82ab7b737cc4c6701e54cda73dcee245fbc5ccbc
duong <duongtde170786@fpt.edu.vn>
2024-05-08 09:17:33 +0700

    buoi1 bai3

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var rect= require('./retangle');
 function solveRect(l,b ){
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
-    if(l<=0 || b<=0){
-        console.log("Rectangle dimensions should be greater than zero: l = " + l + ", and b = " + b);
-    }
-    else ( l>= 0 || B>=0)
-    {
-        console.log("perimeter is "+ rect.perimeter(l,b));
-        console.log("area is "+ rect.area(l,b));
-    }
+    rect(l,b,(err,retangle)=>{
+        if(err){
+            console.log("ERROR "+ err.message);
+        }
+        else 
+        {
+            console.log("perimeter is "+ retangle.perimeter(l,b));
+            console.log("area is "+ retangle.area(l,b));
+        }
+
+    })
 }
 solveRect(2,4);
 solveRect(3,5);
