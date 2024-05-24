1a0b70eade2db468c5ef98a30d4f855a79f8a112
duong <duongtde170786@fpt.edu.vn>
2024-05-24 16:26:16 +0700

    ex9

--- index.js
+++ index.js
@@ -1,39 +1,67 @@
-import { MongoClient } from "mongodb";
+// import pkg from "mongodb";
+const { MongoClient } = require('mongodb');
 
 const url = "mongodb://127.0.0.1:27017";
+const dboper = require('./operation');
 const dbname = "conFusion";
 
+const client = new MongoClient(url);
 async function main() {
-  const client = new MongoClient(url);
 
-  try {
     await client.connect();
     console.log("Connected correctly to server");
 
-    const db = client.db(dbname);
+    const db = client.db('conFusion');
     const collection = db.collection("dishes");
+    dboper.insertDocument(
+        db,
+        { name: "Vadonut", description: "Test" },
+        "dishes",
+        (result) => {
+            console.log("Insert Document:\n", result.ops);
+
+            dboper.findDocuments(db, "dishes", (docs) => {
+                console.log("Found Documents:\n", docs);
+
+                dboper.updateDocument(
+                    db,
+                    { name: "Vadonut" },
+                    { description: "Updated Test" },
+                    "dishes",
+                    (result) => {
+                        console.log("Updated Document:\n", result.result);
+
+                        dboper.findDocuments(db, "dishes", (docs) => {
+                            console.log("Found Updated Documents:\n", docs);
+
+                            db.dropCollection("dishes", (result) => {
+                                console.log("Dropped Collection: ", result);
+
+                                client.close();
+                            });
+                        });
+                    }
+                );
+            });
+        }
+    );
 
     // Insert a document
-    const insertResult = await collection.insertOne({
-      name: "Uthappizza",
-      description: "test",
-    });
-    console.log("After Insert:\n");
-    console.log(insertResult.insertedId);
+    // const insertResult = await collection.insertOne({
+    //   name: "Uthappizza",
+    //   description: "test",
+    // });
+    // console.log("After Insert:\n");
+    // console.log(insertResult.insertedId);
 
-    // Find all documents
-    const docs = await collection.find({}).toArray();
-    console.log("Found:\n");
-    console.log(docs);
+    // // Find all documents
+    // const docs = await collection.find({}).toArray();
+    // console.log("Found:\n");
+    // console.log(docs);
 
-    // Uncomment the following lines to drop the collection
-    // const dropResult = await db.dropCollection("dishes");
-    // console.log("Dropped collection:", dropResult);
-  } catch (err) {
-    console.error(err);
-  } finally {
-    await client.close();
-  }
+    // // Uncomment the following lines to drop the collection
+    // // const dropResult = await db.dropCollection("dishes");
+    // // console.log("Dropped collection:", dropResult);
 }
 
-main().catch(console.error);
+main()
