fa2766b1b518a8469753604649095499d9de0bd6
duong <duongtde170786@fpt.edu.vn>
2024-05-24 16:50:08 +0700

    ex10

--- index.js
+++ index.js
@@ -3,48 +3,82 @@ const { MongoClient } = require('mongodb');
 
 const url = "mongodb://127.0.0.1:27017";
 const dboper = require('./operation');
-const dbname = "conFusion";
+const dbname = 'conFusion';
 
 const client = new MongoClient(url);
-async function main() {
+// async function main() 
+MongoClient.connect(url).then((client)=>{
 
-    await client.connect();
+    // await client.connect();
     console.log("Connected correctly to server");
 
-    const db = client.db('conFusion');
+    const db = client.db(dbname);
     const collection = db.collection("dishes");
     dboper.insertDocument(
         db,
         { name: "Vadonut", description: "Test" },
-        "dishes",
-        (result) => {
+        "dishes")
+        .then((result) => {
             console.log("Insert Document:\n", result.ops);
 
-            dboper.findDocuments(db, "dishes", (docs) => {
-                console.log("Found Documents:\n", docs);
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Documents:\n", docs);
 
-                dboper.updateDocument(
-                    db,
-                    { name: "Vadonut" },
-                    { description: "Updated Test" },
-                    "dishes",
-                    (result) => {
-                        console.log("Updated Document:\n", result.result);
+            return dboper.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes");
 
-                        dboper.findDocuments(db, "dishes", (docs) => {
-                            console.log("Found Updated Documents:\n", docs);
+        })
+        .then((result) => {
+            console.log("Updated Document:\n", result.result);
 
-                            db.dropCollection("dishes", (result) => {
-                                console.log("Dropped Collection: ", result);
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Updated Documents:\n", docs);
+                            
+            return db.dropCollection("dishes");
+        })
+        .then((result) => {
+            console.log("Dropped Collection: ", result);
 
-                                client.close();
-                            });
-                        });
-                    }
-                );
-            });
-        }
-    );
+            return client.close();
+        })
+        .catch((err) => console.log(err));
+
+})
+.catch((err) => console.log(err));
+
+
+        // (result) => {
+        //     console.log("Insert Document:\n", result.ops);
+
+        //     dboper.findDocuments(db, "dishes", (docs) => {
+        //         console.log("Found Documents:\n", docs);
+
+        //         dboper.updateDocument(
+        //             db,
+        //             { name: "Vadonut" },
+        //             { description: "Updated Test" },
+        //             "dishes",
+        //             (result) => {
+        //                 console.log("Updated Document:\n", result.result);
+
+        //                 dboper.findDocuments(db, "dishes", (docs) => {
+        //                     console.log("Found Updated Documents:\n", docs);
+
+        //                     db.dropCollection("dishes", (result) => {
+        //                         console.log("Dropped Collection: ", result);
+
+        //                         client.close();
+        //                     });
+        //                 });
+        //             }
+        //         );
+        //     });
+        // }
+    
 
     // Insert a document
     // const insertResult = await collection.insertOne({
@@ -62,6 +96,6 @@ async function main() {
     // // Uncomment the following lines to drop the collection
     // // const dropResult = await db.dropCollection("dishes");
     // // console.log("Dropped collection:", dropResult);
-}
 
-main()
+
+// main()
