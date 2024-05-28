64210cbe6ed30fa0b326f52f2e1c4048590b3d18
duong <duongtde170786@fpt.edu.vn>
2024-05-28 13:28:37 +0700

    ex12bai1

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ const url= 'mongodb://127.0.0.1:27017/conFusion';
 const connect = mongoose.connect(url);
 connect.then((db)=>{
     console.log("Connect");
-    var newDish= Dishes({
+    // var newDish= Dishes({
+    //     name: 'bún đậu mắm tôm ',
+    //     description : 'món ngon từ mắm tôm '
+    // });
+    // newDish.save()
+    Dishes.create({
         name: 'bún đậu mắm tôm ',
         description : 'món ngon từ mắm tôm '
-    });
-    newDish.save()
+    })
     .then((dish)=>{
         console.log(dish);
-        return Dishes.find({});
+        return Dishes.find({}).exec();
     })
     .then((dishes)=>{
         console.log(dishes);
-        return Dishes.deleteMany();
+        // return Dishes.deleteMany({});
     })
     .then(()=>{
         console.log('Deleted all dishes');
